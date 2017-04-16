ff98dc3c665dd9ec9e2e977374827ea04d5ceacd
n0an <nantog@gmail.com>
2017-04-16 22:19:42 +0300

    GridView class added. Tapping logic added to GridView.
    ViewController: added several testing stones. GridView added grid and interactivity - tap on intersection adding stone.

---
+++ Gomoku/GridView.swift
@@ -0,0 +1,106 @@
+//
+//  GridView.swift
+//  Gomoku
+//
+//  Created by Anton Novoselov on 16/04/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+    
+    let board: Board
+    let boardSize: CGFloat
+    let cellCount: Int
+    let cellSize: CGFloat
+    
+    var tapper: UITapGestureRecognizer!
+    
+    init(frame: CGRect, board: Board) {
+        self.board = board
+        self.boardSize = min(frame.size.width, frame.size.height)
+        
+        self.cellCount = board.WIDTH + 1
+        self.cellSize = self.boardSize / CGFloat(cellCount)
+        
+        super.init(frame: frame)
+        
+        self.tapper = UITapGestureRecognizer(target: self, action: #selector(self.tapped(recognizer:)))
+        self.addGestureRecognizer(self.tapper)
+        
+        self.backgroundColor = UIColor(colorLiteralRed: 255/255, green: 226/255, blue: 124/255, alpha: 1)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
+    
+    func tapped(recognizer: UITapGestureRecognizer) {
+        let locationOfTap = tapper.location(ofTouch: 0, in: self)
+        
+        let tappedColumn = Int(((locationOfTap.x - cellSize) / cellSize) + 0.5)
+        let tappedRow = Int(((locationOfTap.y - cellSize) / cellSize) + 0.5)
+        
+        try! board.place(intersection: Intersection(tappedColumn, tappedRow), player: Player.White)
+        
+        self.setNeedsDisplay()
+        
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        let path = UIBezierPath()
+        
+        for i in 1..<cellCount {
+            let xPos = CGFloat(i) * cellSize
+            path.move(to: CGPoint(x:xPos, y: cellSize))
+            path.addLine(to: CGPoint(x: xPos, y: boardSize - cellSize))
+        }
+        
+        for i in 1..<cellCount {
+            let yPos = CGFloat(i) * cellSize
+            path.move(to: CGPoint(x:cellSize, y: yPos))
+            path.addLine(to: CGPoint(x: boardSize - cellSize, y: yPos))
+        }
+        
+        path.lineWidth = 1
+        path.stroke()
+        
+        for col in 0..<board.WIDTH {
+            for row in 0..<board.HEIGHT {
+                let stone = try! board.get(intersection: Intersection(col, row))
+                
+                if stone != Player.Empty {
+                    let stonePath = UIBezierPath()
+                    
+                    if stone == Player.White {
+                        UIColor.white.setFill()
+                    } else {
+                        UIColor.black.setFill()
+                    }
+                    
+                    let center = CGPoint(x: CGFloat(col + 1) * cellSize, y: CGFloat(row + 1) * cellSize)
+                    let stoneRadius = cellSize / 2.5
+                    
+                    stonePath.move(to: center)
+                    
+                    stonePath.addArc(withCenter: center, radius: stoneRadius, startAngle: 0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
+                    
+                    stonePath.fill()
+                }
+            }
+        }
+        
+        
+       
+    }
+ 
+
+}
+
+
+
+
+
--- Gomoku/ViewController.swift
+++ Gomoku/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var board = Board()
+        try! board.place(intersection: Intersection(10, 10), player: Player.White)
+        try! board.place(intersection: Intersection(11, 11), player: Player.Black)
+        try! board.place(intersection: Intersection(8, 8), player: Player.White)
+        try! board.place(intersection: Intersection(9, 9), player: Player.Black)
+
+
+        
+        let gridRect = CGRect(x: 0, y: 200, width: self.view.frame.width, height: self.view.frame.width)
+        
+        let gridView = GridView(frame: gridRect, board: board)
+        
+        view.addSubview(gridView)
+        
     }
 
     override func didReceiveMemoryWarning() {
