62a9054ecaca6ec27ed45633e7c21eac048bc67d
n0an <nantog@gmail.com>
2017-04-16 13:22:36 +0300

    Init commit

---
+++ Gomoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gomoku
+//
+//  Created by Anton Novoselov on 16/04/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GomokuTests/BoardTest.swift
@@ -0,0 +1,15 @@
+//
+//  BoardTest.swift
+//  Gomoku
+//
+//  Created by Anton Novoselov on 16/04/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import XCTest
+
+class BoardTest: XCTestCase {
+    
+    
+    
+}
